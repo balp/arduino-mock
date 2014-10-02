b5a11bc12ce74aca69f6a050a8b01504cb3c3637
Hoang Nguyen <huahoang2112@gmail.com>
2014-10-02 07:04:23 +0100

    Minor cosmetic change to wire.h, wire.cc and wire_unittest.cc

--- include/arduino-mock/wire.h
+++ include/arduino-mock/wire.h
@@ -11,35 +11,34 @@
 class TwoWire
 {
   public:
-	void begin();
-	void begin(uint8_t);
-	void begin(int);
-	void beginTransmission(uint8_t);
-	void beginTransmission(int);
-	uint8_t endTransmission(void);
+    void begin();
+    void begin(uint8_t);
+    void begin(int);
+    void beginTransmission(uint8_t);
+    void beginTransmission(int);
+    uint8_t endTransmission(void);
     uint8_t endTransmission(uint8_t);
-	uint8_t requestFrom(uint8_t, uint8_t);
+    uint8_t requestFrom(uint8_t, uint8_t);
     uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
 
     inline size_t write(unsigned long n) { return write((uint8_t)n); }
     inline size_t write(long n) { return write((uint8_t)n); }
     inline size_t write(unsigned int n) { return write((uint8_t)n); }
     inline size_t write(int n) { return write((uint8_t)n); }
-//	using Print::write;
 };
 
 class TwoWireMock
 {
-	public:
-		MOCK_METHOD0(begin, void());
-		MOCK_METHOD1(begin, void(uint8_t));
-		MOCK_METHOD1(begin, void(int));
-		MOCK_METHOD1(beginTransmission, void(uint8_t));
-		MOCK_METHOD1(beginTransmission, void(int));
-		MOCK_METHOD0(endTransmission, uint8_t(void));
-		MOCK_METHOD1(endTransmission, uint8_t(uint8_t));
-		MOCK_METHOD2(requestFrom, uint8_t(uint8_t, uint8_t));
-		MOCK_METHOD3(requestFrom, uint8_t(uint8_t, uint8_t, uint8_t));
+  public:
+    MOCK_METHOD0(begin, void());
+    MOCK_METHOD1(begin, void(uint8_t));
+    MOCK_METHOD1(begin, void(int));
+    MOCK_METHOD1(beginTransmission, void(uint8_t));
+    MOCK_METHOD1(beginTransmission, void(int));
+    MOCK_METHOD0(endTransmission, uint8_t(void));
+    MOCK_METHOD1(endTransmission, uint8_t(uint8_t));
+    MOCK_METHOD2(requestFrom, uint8_t(uint8_t, uint8_t));
+    MOCK_METHOD3(requestFrom, uint8_t(uint8_t, uint8_t, uint8_t));
 };
 
 TwoWireMock* TwoWireMockInstance();
