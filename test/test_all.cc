986ef081b741e7b50c725cef999846a7257980a9
ikeyasu <ikeyasu@gmail.com>
2015-05-24 15:11:01 +0900

    Support ctest

---
+++ test/test_all.cc
@@ -0,0 +1,12 @@
+#include "Arduino_unittest.cc"
+#include "EEPROM_unittest.cc"
+#include "Serial_unittest.cc"
+#include "serialHelper_unittest.cc"
+#include "Spark_unittest.cc"
+#include "WiFi_unittest.cc"
+#include "Wire_unittest.cc"
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
