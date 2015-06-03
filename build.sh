93b53d6dec991d2c26d6e0ee7205cc8e8398ba3b
redaphid <a@octoblu.com>
2015-06-02 22:11:39 -0700

    included the external-project-utils...externally. whoa.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 [ ! -d build ] && mkdir build
+[ -e build/external-project-utils.cmake ] && rm build/external-project-utils.cmake
+
 cd build && \
-cmake  .. -Dtest=ON && \
+wget https://raw.githubusercontent.com/octoblu/cmake-external-project-utils/master/external-project-utils.cmake && \
+cmake  .. && \
 make VERBOSE=1 && \
 ctest -VV
