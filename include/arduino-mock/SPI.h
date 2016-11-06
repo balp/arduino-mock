f4e7ea9ef1a905e7fae8f79d5df530d4b46286c4
Anner J. Bonilla <anner.bonilla@hpe.com>
2016-11-06 19:37:20 -0400

    added SPI Fixed gtest download link Fixed gmock download links

---
+++ include/arduino-mock/SPI.h
@@ -0,0 +1,57 @@
+// Header for SPI Mock
+
+#ifndef __SPI_h__
+#define __SPI_h__
+
+#include <gmock/gmock.h>
+#include <stdint.h>
+
+class SPISettings
+{
+	public:
+		SPISettings(uint32_t , uint8_t , uint8_t );
+		SPISettings()
+};
+
+class SPI_ {
+  public:
+    void begin();
+	void usingInterrupt(uint8_t )
+	void notUsingInterrupt(uint8_t );
+	void beginTransaction(SPISettings );
+	uint8_t transfer(uint8_t );
+	uint16_t transfer16(uint16_t );
+	void transfer(void *, size_t );
+	void endTransaction(void);
+	void end();
+	void setBitOrder(uint8_t);
+	void setDataMode(uint8_t );
+	void setClockDivider(uint8_t clockDiv);
+	void attachInterrupt();
+	void detachInterrupt();
+};
+
+extern SPI_ SPI;
+
+class SPIMock {
+  public:
+    MOCK_METHOD0(begin, void());
+    MOCK_METHOD1(usingInterrupt, void(uint8_t));
+    MOCK_METHOD1(notUsingInterrupt, void(uint8_t));
+    MOCK_METHOD1(beginTransaction, void(SPISettings));
+    MOCK_METHOD1(transfer, uint8_t(uint8_t));
+	MOCK_METHOD1(transfer16, uint16_t(uint16_t));
+    MOCK_METHOD2(transfer, void(void *, size_t));
+    MOCK_METHOD0(endTransaction, void());
+    MOCK_METHOD0(end, void());
+    MOCK_METHOD1(setBitOrder, void(uint8_t));
+    MOCK_METHOD1(setDataMode, void(uint8_t));
+    MOCK_METHOD1(setClockDivider, void(uint8_t));
+    MOCK_METHOD0(attachInterrupt, void());
+    MOCK_METHOD0(detachInterrupt, void());
+};
+
+SPIMock* SPIMockInstance();
+void releaseSPIMock();
+
+#endif
---
+++ test/SPI_unittest.cc
@@ -0,0 +1,49 @@
+#include "gtest/gtest.h"
+#include "arduino-mock/SPI.h"
+#include "arduino-mock/Arduino.h"
+
+using ::testing::Return;
+TEST(SPI, access) {
+  uint8_t value1 = 10;
+  uint8_t value2 = 12;
+  uint16_t value = 3;
+  char text[] = "Mock test";
+  uint8_t* callback_func = NULL;
+  void* voidTest = NULL;
+  
+  SPISettings settings;
+  WireMock* mock = SPIMockInstance();
+  
+  EXPECT_CALL(*mock, begin());
+  EXPECT_CALL(*mock, usingInterrupt(value1));
+  EXPECT_CALL(*mock, notUsingInterrupt(value1));
+  EXPECT_CALL(*mock, beginTransaction(settings));
+  EXPECT_CALL(*mock, transfer(value1));
+  EXPECT_CALL(*mock, transfer16(value));
+  EXPECT_CALL(*mock, transfer(voidTest,value1));
+  EXPECT_CALL(*mock, endTransaction());
+  EXPECT_CALL(*mock, end());
+  EXPECT_CALL(*mock, setBitOrder(value1));
+  EXPECT_CALL(*mock, setDataMode(value1));
+  EXPECT_CALL(*mock, setClockDivider(value1));
+  EXPECT_CALL(*mock, attachInterrupt());
+  EXPECT_CALL(*mock, detachInterrupt());
+  
+  
+  SPI.begin());
+  SPI.usingInterrupt(value1);
+  SPI.notUsingInterrupt(value1);
+  SPI.beginTransaction(settings);
+  SPI.transfer(value1);
+  SPI.transfer16(value);
+  SPI.transfer(voidTest,value1);
+  SPI.endTransaction());
+  SPI.end());
+  SPI.setBitOrder(value1);
+  SPI.setDataMode(value1);
+  SPI.setClockDivider(value1);
+  SPI.attachInterrupt());
+  SPI.detachInterrupt());
+  
+  releaseSPIMock();
+}
