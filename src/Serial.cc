09f4f0bc224b88aad26facffa3ae346ae21b5e96
Adrián Roselló <adrian.rosello@witrac.es>
2018-07-11 17:30:58 +0200

    Fake implementation of the serial interface, capable of simulating RX

--- src/Serial.cc
+++ src/Serial.cc
@@ -222,3 +222,28 @@ uint8_t Serial_::read() {
 
 // Preinstantiate Objects
 Serial_ Serial;
+
+void SerialFake::buffer_load(uint8_t buffer_0[], const uint8_t len) {
+    assert (len <= buffer_size);
+
+    memcpy(buffer, buffer_0, len);
+    buffer_head = 0;
+    buffer_tail = len;
+}
+
+uint8_t SerialFake::available() {
+    return (buffer_tail - buffer_head);
+}
+
+uint8_t SerialFake::read()
+{
+    assert(buffer_tail - buffer_head > 0);
+
+    return buffer[buffer_head++];
+}
+
+uint8_t SerialFake::at(const uint8_t index) {
+    assert(buffer_head + index < buffer_tail);
+
+    return buffer[buffer_head + index];
+}
