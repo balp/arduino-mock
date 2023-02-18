1bbc9e0da938d8c16ae6a201db8c6bd06d6a4648
sidey79 <7968127+sidey79@users.noreply.github.com>
2023-02-18 15:04:21 +0100

    Arduino.h
    Added bitmanipulation macros

--- include/arduino-mock/Arduino.h
+++ include/arduino-mock/Arduino.h
@@ -110,6 +110,13 @@ extern "C" {
 #define lowByte(w) ((uint8_t) ((w) & 0xff))
 #define highByte(w) ((uint8_t) ((w) >> 8))
 
+//bit manipulation macros
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#define bit(b) (1UL << (b))
+
 typedef uint8_t boolean;
 typedef uint8_t byte;
 
